79dc4ab48069c83bd5d054d67b3b6463737fe178
Huhainz <>
2025-10-24 15:43:46 +0200

    Optimisation

--- RoidedUNO/uno-main.cpp
+++ RoidedUNO/uno-main.cpp
@@ -10,8 +10,6 @@ using namespace std;
 enum ColorPrint {black, white};
 enum NumberPrint{zero, one, two, three, four, five, six, seven, eight, nine};
 
-const int numbers[10] ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-const string colors[2] = {"black" , "white"};
 class Card
 {
     public:
@@ -20,6 +18,7 @@ class Card
         string Display;
 };
 
+// Give cards functions
 void GiveRandomCard (vector<Card> &Hand)
 {
     Card tempCard;
@@ -37,15 +36,10 @@ void PutRandomCard(Card &Table)
     Table.Number = static_cast<NumberPrint>(indexNumber);
     Table.Color = static_cast<ColorPrint>(indexColor);
 }
-int main() 
-{
-    srand(time(nullptr));
-    vector<Card> PlayerHand;
-    Card Table;
-    PutRandomCard(Table);
-    GiveRandomCard(PlayerHand);
 
-    cout << "Card on the table: ";
+// Display functions 
+void DisplayCard(vector<Card> PlayerHand)
+{
     switch(PlayerHand[0].Color)
     {
         case black: cout << "Black "; break;
@@ -64,6 +58,35 @@ int main()
         case eight : cout << "8" << endl; break; 
         case nine : cout << "9" << endl; break;
     }
+}
+
+int main() 
+{
+    srand(time(nullptr));
+    vector<Card> PlayerHand;
+    Card Table;
+    PutRandomCard(Table);
+    GiveRandomCard(PlayerHand);
+    DisplayCard(PlayerHand);
+    // cout << "Card on the table: ";
+    // switch(PlayerHand[0].Color)
+    // {
+    //     case black: cout << "Black "; break;
+    //     case white: cout << "White "; break;
+    // }
+    // switch(PlayerHand[0].Number)
+    // {
+    //     case zero : cout << "0" << endl; break;
+    //     case one : cout << "1" << endl; break;
+    //     case two : cout << "2" << endl; break;
+    //     case three : cout << "3" << endl; break;
+    //     case four : cout << "4" << endl; break;
+    //     case five : cout << "5" << endl; break;
+    //     case six : cout << "6" << endl; break;
+    //     case seven : cout << "7" << endl; break;
+    //     case eight : cout << "8" << endl; break; 
+    //     case nine : cout << "9" << endl; break;
+    // }
     // cout << PlayerHand[0].Color << " " << PlayerHand[0].Number  << endl ;
     // if (PlayerHand[0].Color == Table.Color || PlayerHand[0].Number == Table.Number)
     // {
