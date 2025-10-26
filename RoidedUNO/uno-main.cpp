d839884b0271a478b453d2cf402304ee43449106
Huhainz <>
2025-10-26 12:48:06 +0100

    Max count added for each card aswell as a Deck manager

--- RoidedUNO/uno-main.cpp
+++ RoidedUNO/uno-main.cpp
@@ -2,25 +2,27 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <map>
 #include <cstdlib>   // For randomizing
 #include <ctime>     // For randomizing
 #include <windows.h> // Terminal colors
 
 using namespace std;
 
-enum ColorPrint {red, yellow, green, blue};
+enum ColorPrint {red, yellow, green, blue, wild};
 const int ColorPrint_len = 4;
-enum NumberPrint{zero, one, two, three, four, five, six, seven, eight, nine, plustwo, skip, reverse};
+enum NumberPrint{zero, one, two, three, four, five, six, seven, eight, nine, plustwo, skip, reverse, wild, plusfour};
 const int NumberPrint_len = 13;
+
 // Deck cards
 const int NormalN = 2;
 const int DrawN = 2;
 const int ReverseN = 2;
 const int ZeroN = 1;
+const int SkipN = 2;
 
 const int ColorN = ZeroN + NormalN + DrawN + ReverseN + SkipN;
 
-const int SkipN = 2;
 const int WildsN = 4;
 
 class Card
@@ -31,16 +33,50 @@ class Card
         string Display;
 };
 
+
+class DeckManager 
+{
+    public:
+        map<pair<ColorPrint, NumberPrint>, int> deckCount;
+        map<pair<ColorPrint, NumberPrint>, int> maxCount;
+        DeckManager()
+        {
+            for (int c=0; c < ColorPrint_len; c++)
+            {
+                ColorPrint color = static_cast<ColorPrint>(c);
+                maxCount[{color,zero}] = ZeroN;
+                for (int n = one; n <= nine; n++)
+                    maxCount[{color,static_cast<NumberPrint>(n)}] = NormalN;
+                maxCount[{color, plustwo}] = DrawN;
+                maxCount[{color, skip}] = SkipN;
+                maxCount[{color, NumberPrint::reverse}] = ReverseN;
+            }
+        for (auto &init : maxCount)
+            deckCount[init.first] = 0;
+        }
+        bool canDraw (ColorPrint c, NumberPrint n)
+        {
+            return deckCount[{c,n}] < maxCount[{c,n}];
+        }
+};
+
 // Give cards functions
-void GiveRandomCard (vector<Card> &Hand)
+void GiveRandomCard (vector<Card> &Hand, DeckManager &Deck)
 {
     Card tempCard;
     int indexNumber = rand() % NumberPrint_len;
     int indexColor = rand() % ColorPrint_len;
     tempCard.Number = static_cast<NumberPrint>(indexNumber);
-    tempCard.Color = static_cast<ColorPrint>(indexColor);
-    Hand.push_back(tempCard);
+    tempCard.Color =  static_cast<ColorPrint>(indexColor);
+    if (Deck.canDraw(tempCard.Color, tempCard.Number))
+    {
+        Hand.push_back(tempCard);
+        Deck.deckCount[{tempCard.Color, tempCard.Number}]++;
+    }
+    else
+        GiveRandomCard(Hand, Deck);
 }
+// void GiveRandomWildCard
 
 void PutRandomCard(Card &Table)
 {
@@ -73,6 +109,9 @@ void DisplayCard(vector<Card> PlayerHand, int CardIndex)
         case seven : cout << "7" << endl; break;
         case eight : cout << "8" << endl; break; 
         case nine : cout << "9" << endl; break;
+        case plustwo : cout << "+2" << endl; break;
+        case skip : cout << "skip" << endl; break;
+        case NumberPrint::reverse : cout << "reverse" << endl; break;
     }
     SetConsoleTextAttribute(hConsole,0);
 }
@@ -81,13 +120,17 @@ int main()
 {
     srand(time(nullptr));
     vector<Card> PlayerHand;
+    DeckManager Deck;
     Card Table;
     PutRandomCard(Table);
     for (int i = 0; i < 7; i++)
     {
-        GiveRandomCard(PlayerHand);
+        GiveRandomCard(PlayerHand, Deck);
+        // randomize between random normal card and random wildcard
         DisplayCard(PlayerHand, i);
     }
+
+
 
 
 
